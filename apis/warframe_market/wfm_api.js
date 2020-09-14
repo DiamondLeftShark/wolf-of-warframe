4f6121480ae58bec2b98e0478d7d38696db71a73
Reed Brooks <reedb36@gmail.com>
2020-09-13 17:25:36 -0700

    Work on data retrieval from WFM API

--- apis/warframe_market/wfm_api.js
+++ apis/warframe_market/wfm_api.js
@@ -1,25 +1,41 @@
 //file for handling API calls to Warframe Market
-//NOTE: WFM has a requested a limit of 3 API calls/second
+
 const axios = require('axios');
-var bodyParser = require('body-parser');
+const rateLimit = require('axios-rate-limit');
 
 //root URL for API requests
 const wfmUrl = 'https://api.warframe.market/v1/';
+//NOTE: WFM has a requested a limit of 3 API calls/second
+const http = rateLimit(axios.create(), { maxRPS: 3 });
 
 //retrieves basic info on the items listed in WFM and returns it to the requestor.
 //This should be called first when requesting basic item info.
 var getItems = function(callback) {
   let url = wfmUrl.concat('items');
 
-  axios.get(url)
+  http.get(url)
   .then(function(response) {
-    //TBD: send data back to requestor
-    callback(response.data);
+    callback(response.data.payload.items);
   })
   .catch(function(error) {
-    //TBD: error handlling
     console.log(error);
+    callback('ERROR');
   })
 }
 
+//get ducat information
+var getDucats = function(callback) {
+  let url = wfmUrl.concat('tools/ducats');
+
+  http.get(url)
+  .then(function(response) {
+    callback(response.data.payload.previous_hour);
+  })
+  .catch(function(error) {
+    console.log(error);
+    callback('ERROR');
+  });
+}
+
 module.exports.getItems = getItems;
+module.exports.getDucats = getDucats;
--- database/index.js
+++ database/index.js
@@ -8,10 +8,26 @@ const wfm = require('../apis/warframe_market/wfm_api.js');
 //connect to WFM and populate database with latest data
 var loadData = function(callback) {
   console.log("Downloading item data from Warframe Market...");
+
   wfm.getItems((itemData) => {
-    console.log("Data received!");
+    if(itemData === 'ERROR') {
+      console.log("Error getting item data!");
+    } else {
+    console.log("Basic item data received!");
     console.log(itemData);
-  })
+
+    console.log("Getting ducat information...");
+
+    wfm.getDucats((ducatData) => {
+      if(ducatData === 'ERROR') {
+        console.log("Error getting ducat data!");
+      } else {
+        console.log("Ducat data received!");
+        console.log(ducatData);
+      }
+    })
+    }
+  });
 }
 
 module.exports.loadData = loadData;
