18efed21f2e7822e764dc74f0ace9e5bf96bcaa8
Reed Brooks <reedb36@gmail.com>
2020-09-14 10:31:56 -0700

    Basic container skeleton created

---
+++ client/src/container.jsx
@@ -0,0 +1,16 @@
+/*Main Container: loaded into HTML document, and is in charge of rendering submodules/menus*/
+
+import React from 'react';
+
+class Container extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render() {
+    return(<h1>Testing!</h1>);
+  }
+};
+
+export default Container;
