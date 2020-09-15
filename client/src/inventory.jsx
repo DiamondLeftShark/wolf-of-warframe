014a22d0c97afe205ce040d9871d6a4e3bee1ebf
Reed Brooks <reedb36@gmail.com>
2020-09-14 19:33:28 -0700

    Update inventory to use modifiable field for quantity

--- client/src/inventory.jsx
+++ client/src/inventory.jsx
@@ -26,7 +26,7 @@ class Inventory extends React.Component {
         output.push(<div>
                       <img src={thumbnailUrl}/>
                       <div>{this.props.inventory[i].item_name}</div>
-                      <div>Quantity: {this.props.inventory[i].quantity}</div>
+                      <div>Quantity: <input type="number" min="0" max="99999" step="1" defaultValue={this.props.inventory[i].quantity}></input></div>
                     </div>);
       }
       return output;
